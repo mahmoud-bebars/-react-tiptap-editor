18b9a78922bde4abeef3534ce719b07b7a23ae53
m.bebars <66588352+mahmoud-bebars@users.noreply.github.com>
2023-07-12 23:40:23 +0300

    first init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Editor from "./Editor";
+import React from "react";
 function App() {
+  const [editorContent, setEditorContent] = React.useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col items-center justify-center gap-4">
+      <Editor content={editorContent} setContent={setEditorContent} />
+      <button
+        className="bg-blue-600 p-3  mt-4 text-white rounded-xl"
+        onClick={() => console.log(editorContent)}
+      >
+        console content
+      </button>
     </div>
   );
 }
